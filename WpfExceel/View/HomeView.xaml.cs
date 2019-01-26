39123bf28489bf453781cb05111983a15cd3131a
hamdiChehdi <34605324+hamdiChehdi@users.noreply.github.com>
2019-01-26 20:36:17 +0100

    Add Home page to import the excel file

---
+++ WpfExceel/View/HomeView.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace WpfExceel.View
+{
+    using System.Windows;
+    using WpfExceel.ViewModel;
+
+    public partial class HomeView : Window
+    {
+        public HomeView()
+        {
+            this.InitializeComponent();
+            this.DataContext = new HomeViewModel();
+        }
+    }
+}
---
+++ WpfExceel/ViewModel/HomeViewModel.cs
@@ -0,0 +1,53 @@
+﻿namespace WpfExceel.ViewModel
+{
+    using System;
+    using System.IO;
+    using System.Windows.Input;
+    using Microsoft.Win32;
+    using WpfExceel.MvvmInfrastructure;
+
+    public class HomeViewModel : ViewModelBase
+    {
+        private string filePath;
+
+        public HomeViewModel()
+        {
+
+        }
+
+        public string FilePath
+        {
+            get
+            {
+                return this.filePath;
+            }
+
+            set
+            {
+                this.filePath = value;
+                this.NotifyPropertyChanged("FilePath");
+            }
+        }
+
+        public ICommand ImportExcelCmd
+        {
+            get
+            {
+                return new DelegateCommand(new Action<object>(this.ImportExcel));
+            }
+        }
+
+        private void ImportExcel(object input)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Excel Worksheets|*.xls";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                this.FilePath = openFileDialog.FileNames[0];
+            }
+        }
+    }
+}
